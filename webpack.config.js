ed0c2548e1fea6cc7f47737f14001a1be7220b62
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2025-05-15 00:57:33 +0300

    Git init

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', 
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          'style-loader',
+          'css-loader', 
+          'sass-loader' 
+        ]
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html' 
+    })
+  ],
+  devServer: {
+    static: './dist',
+    port: 3000,
+    open: true
+  },
+  mode: 'development'
+};
