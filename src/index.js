08e8f442297ba608df3b141e739d8991e49b66ed
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2025-05-16 18:48:09 +0300

    minor card changes

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createPtoductCards } from './components/ProductCards/product-cards'
 
 export function renderMainPage() {
   const app = document.querySelector('#app');
-  const urlVideo = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+  const urlVideo = 'https://youtu.be/B9VRvOKKwfs?si=TFCC7pFdKWTDPDP6';
 
   const videoElement = createVideoPlayer(urlVideo);
   app.appendChild(videoElement);
